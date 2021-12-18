639ddc4944179c3c71a750357e7ce46af3bf69bb
raffal1993 <raffal1993@gmail.com>
2021-12-18 21:04:04 +0100

    update Dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -2,14 +2,16 @@ import Header from "components/Header/Header";
 import Navbar from "components/Navbar/Navbar";
 import React from "react";
 import { Wrapper } from "./Dashboard.style";
+import CountriesList from "components/CountriesList/CountriesList";
 
 const Dashboard = () => {
-     return (
-          <Wrapper>
-               <Header></Header>
-               <Navbar></Navbar>
-          </Wrapper>
-     );
+    return (
+        <Wrapper>
+            <Header />
+            <Navbar />
+            <CountriesList />
+        </Wrapper>
+    );
 };
 
 export default Dashboard;
