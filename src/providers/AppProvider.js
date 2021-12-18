3bd5ac1e6cc3eca292897d0b2256700f430a7c1a
raffal1993 <raffal1993@gmail.com>
2021-12-18 21:04:37 +0100

    add CountriesProvider

--- src/providers/AppProvider.js
+++ src/providers/AppProvider.js
@@ -2,14 +2,17 @@ import React from "react";
 import { GlobalStyle } from "../assets/styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../assets/styles/theme";
+import CountriesProvider from "./CountriesProvider";
 
 const AppProvider = ({ children }) => {
-     return (
-          <ThemeProvider theme={theme}>
-               <GlobalStyle />
-               {children}
-          </ThemeProvider>
-     );
+    return (
+        <CountriesProvider>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                {children}
+            </ThemeProvider>
+        </CountriesProvider>
+    );
 };
 
 export default AppProvider;
