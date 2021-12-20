db7e9d25a3d211c7bd1a78cdb533208e9056ba59
raffal1993 <raffal1993@gmail.com>
2021-12-20 13:18:37 +0100

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import AppProvider from "./providers/AppProvider";
 import Dashboard from "views/Dashboard";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-     return (
-          <AppProvider>
-               <Dashboard />
-          </AppProvider>
-     );
+    return (
+        <BrowserRouter>
+            <AppProvider>
+                <Dashboard />
+            </AppProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
