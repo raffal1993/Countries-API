88caeb8db77a9c0693784369a6c9a3ab89f83d44
raffal1993 <raffal1993@gmail.com>
2021-12-23 20:42:32 +0100

    adding state to sessionStorage handler

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "actions/inputs";
 import { countriesApi } from "actions/fetchCountries";
+import { loadState, saveState } from "./sessionStorage";
+import { throttle } from "lodash";
 
 export const store = configureStore({
+    preloadedState: loadState(),
     reducer: {
         countries: countriesReducer,
         [countriesApi.reducerPath]: countriesApi.reducer
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(countriesApi.middleware)
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(countriesApi.middleware);
+    }
 });
+
+store.subscribe(
+    throttle(() => {
+        saveState(store.getState());
+    }, 500)
+);
