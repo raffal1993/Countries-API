0b7728d0d0d2db67d1265ec86f7bd34039a32868
raffal1993 <raffal1993@gmail.com>
2021-12-23 20:43:05 +0100

    add Routes

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,15 +1,26 @@
 import Header from "components/Header/Header";
 import Navbar from "components/Navbar/Navbar";
+import CountryPage from "components/CountryPage/CountryPage";
 import React from "react";
 import { Wrapper } from "./Dashboard.style";
 import CountriesList from "components/CountriesList/CountriesList";
+import { Routes, Route } from "react-router-dom";
 
 const Dashboard = () => {
     return (
         <Wrapper>
             <Header />
-            <Navbar />
-            <CountriesList />
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <>
+                            <Navbar />, <CountriesList />
+                        </>
+                    }
+                />
+                <Route path='/country/:code' element={<CountryPage />} />
+            </Routes>
         </Wrapper>
     );
 };
