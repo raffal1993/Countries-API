9ac852dc9d62f90b5bd819c92181aef124b947cc
raffal1993 <raffal1993@gmail.com>
2021-12-23 20:43:25 +0100

    add CountryInfo component

---
+++ src/components/CountryPage/CountryInfo/CountryInfo.jsx
@@ -0,0 +1,116 @@
+import { setCountryCode } from "actions/inputs";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { Button } from "../CountryPage.style";
+import { CountryInfo } from "./CountryInfo.style";
+import { Content } from "./CountryInfo.style";
+import { useParams } from "react-router-dom";
+
+const CountryInfoList = ({ data }) => {
+    const {
+        flags,
+        name,
+        nativeName,
+        population,
+        region,
+        subregion,
+        capital,
+        topLevelDomain,
+        currencies,
+        languages,
+        borders
+    } = data;
+
+    const { code } = useParams();
+
+    const dispatch = useDispatch();
+
+    console.log(code);
+
+    useEffect(() => {
+        dispatch(setCountryCode(code));
+    }, [code, dispatch]);
+
+    const handleShowCountry = (code) => {
+        dispatch(setCountryCode(code));
+    };
+
+    const showInfo = (info) => {
+        if (!info) return <span>Not found</span>;
+        if (!Array.isArray(info)) return info;
+
+        return info.map((el, index) => (
+            <React.Fragment key={el.name || el}>
+                [{el.name || el}]{info.length - 1 !== index && `, `}
+                {""}
+            </React.Fragment>
+        ));
+    };
+
+    return (
+        <Content>
+            <div className='img-container'>
+                <img src={flags.png} alt='' />
+            </div>
+            <CountryInfo>
+                <h1>{name}</h1>
+                <ul>
+                    <li>
+                        <strong>Native Name: </strong>
+                        {showInfo(nativeName)}
+                    </li>
+                    <li>
+                        <strong>Population: </strong>
+                        {showInfo(population)}
+                    </li>
+                    <li>
+                        <strong>Region: </strong>
+                        {showInfo(region)}
+                    </li>
+                    <li>
+                        <strong>Sub Region: </strong>
+                        {showInfo(subregion)}
+                    </li>
+                    <li>
+                        <strong>Capital: </strong>
+                        {showInfo(capital)}
+                    </li>
+                    <li>
+                        <strong>Top Level Domain: </strong>
+                        {showInfo(topLevelDomain)}
+                    </li>
+                    <li>
+                        <strong>Currencies: </strong>
+                        {showInfo(currencies)}
+                    </li>
+                    <li>
+                        <strong>Languages: </strong>
+                        {showInfo(languages)}
+                    </li>
+                </ul>
+                <div className='borders'>
+                    <h3>Border countries: </h3>
+                    <div className='buttons-container'>
+                        {borders ? (
+                            borders.map((border) => (
+                                <Button
+                                    onClick={() => handleShowCountry(border)}
+                                    key={border}
+                                >
+                                    <Link to={`/country/${border}`}>
+                                        {border}
+                                    </Link>
+                                </Button>
+                            ))
+                        ) : (
+                            <span>[Not found]</span>
+                        )}
+                    </div>
+                </div>
+            </CountryInfo>
+        </Content>
+    );
+};
+
+export default CountryInfoList;
---
+++ src/components/CountryPage/CountryInfo/CountryInfo.style.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import { Button } from "../CountryPage.style";
+
+export const Content = styled.div`
+    display: flex;
+    width: 100%;
+
+    .img-container {
+        width: 40%;
+
+        img {
+            width: 100%;
+            object-fit: contain;
+            max-height: 70%;
+        }
+    }
+`;
+
+export const CountryInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px 0;
+    padding-left: 60px;
+    color: ${({ theme }) => theme.colors.white};
+    width: 60%;
+    height: 100%;
+
+    h1 {
+        font-size: 30px;
+        margin-bottom: 40px;
+    }
+
+    ul {
+        font-size: ${({ theme }) => theme.fontSize.m};
+        display: flex;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        flex-direction: column;
+        column-gap: 30px;
+        letter-spacing: 1px;
+        min-height: 0;
+        height: 60%;
+
+        li {
+            margin-bottom: 15px;
+        }
+
+        span {
+            font-style: italic;
+        }
+    }
+    .borders {
+        padding-top: 60px;
+        display: flex;
+        align-items: center;
+
+        h3 {
+            margin-right: 15px;
+        }
+
+        .buttons-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            /* flex-basis: auto; */
+
+            ${Button} {
+                margin: 5px;
+            }
+
+            span {
+                font-style: italic;
+                margin-left: 20px;
+            }
+        }
+    }
+`;
