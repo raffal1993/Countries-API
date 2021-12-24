bed16678b9c6272fa1705504c82aec6ee27be5c8
raffal1993 <raffal1993@gmail.com>
2021-12-24 13:36:08 +0100

    set up dark/light mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,25 @@
+import { useMode } from "hooks/useMode";
 import React, { useState } from "react";
 import { Mode, Slider, Wrapper } from "./Header.style";
 
 const Header = () => {
-     const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const { mode, toggleMode } = useMode();
 
-     const modeHandler = () => {
-          setIsDarkMode((prev) => !prev);
-     };
+    const modeHandler = () => {
+        setIsDarkMode((prev) => !prev);
+        toggleMode();
+    };
 
-     return (
-          <Wrapper>
-               <h2>Where in the world?</h2>
-               <Mode>
-                    <Slider
-                         isDarkMode={isDarkMode}
-                         onClick={modeHandler}
-                    ></Slider>
-                    <span> Dark Mode</span>
-               </Mode>
-          </Wrapper>
-     );
+    return (
+        <Wrapper>
+            <h2>Where in the world?</h2>
+            <Mode>
+                <Slider isDarkMode={isDarkMode} onClick={modeHandler}></Slider>
+                <span> Dark Mode</span>
+            </Mode>
+        </Wrapper>
+    );
 };
 
 export default Header;
