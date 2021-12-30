21dc28e95f746f420c53e66bc8531fed0dd47898
raffal1993 <raffal1993@gmail.com>
2021-12-30 18:36:13 +0100

    add redirect on logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,29 @@ import { useMode } from "hooks/useMode";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Mode, Slider, Wrapper } from "./Header.style";
+import { useDispatch } from "react-redux";
+import { setCountries } from "actions/countries";
+import { useGetAllCountriesQuery } from "actions/fetchData";
+import { setName, setRegion } from "actions/inputs";
 
 const Header = () => {
     const { mode, toggleMode } = useMode();
 
+    const dispatch = useDispatch();
+
+    const { data: dataAll } = useGetAllCountriesQuery();
+
+    const resetHandle = () => {
+        dispatch(setName(``));
+        dispatch(setRegion(``));
+        setTimeout(() => {
+            dispatch(setCountries(dataAll));
+        }, 0);
+    };
+
     return (
         <Wrapper>
-            <Link as='h2' to='/'>
+            <Link as='h2' to='/' onClick={resetHandle}>
                 Where in the world?
             </Link>
             <Mode>
